c2672e8f8f8575270d616885dbe9c8140d807bae
romankuvshanov <83648973+romankuvshanov@users.noreply.github.com>
2023-08-01 16:08:27 +0500

    Сделал папки и таски (визуал)

--- src/components/FoldersSection/FoldersSection.jsx
+++ src/components/FoldersSection/FoldersSection.jsx
@@ -1,5 +1,55 @@
-import './FoldersSection.scss';
+import "./FoldersSection.scss";
+import Input from "../inputs/Input/Input";
+import FolderPlusIcon from "../icons/FolderPlusIcon/FolderPlusIcon";
+import ArchiveIcon from "../icons/ArchiveIcon/ArchiveIcon";
+import CloseIcon from "../icons/CloseIcon/CloseIcon";
 
 export default function FoldersSection() {
-    return <div className={'folders-section'}></div>
+  return (
+    <div className={"folders-section"}>
+      <Input placeholder={"Добавить папку"}>
+        <FolderPlusIcon />
+      </Input>
+
+      {folders.map((folder) => {
+        return (
+          <div className={"folders-section__folder"} key={folder.id}>
+            <p className={"folders-section__paragraph"}>{folder.name}</p>
+            <CloseIcon />
+          </div>
+        );
+      })}
+
+      <div className={"folders-section__folder"}>
+        <p className={"folders-section__paragraph"}>Основные задачи</p>
+      </div>
+
+      <div
+        className={"folders-section__folder folders-section__folder--archive"}
+      >
+        <p className={"folders-section__paragraph"}>
+          <ArchiveIcon />
+          Архив
+        </p>
+      </div>
+    </div>
+  );
 }
+
+const folders = [
+  {
+    name: "Аутсорс",
+    tasks: [],
+    id: 0,
+  },
+  {
+    name: "Задачи на февраль",
+    tasks: [],
+    id: 1,
+  },
+  {
+    name: "Задачи дениса",
+    tasks: [],
+    id: 2,
+  },
+];
--- src/components/TasksSection/TasksSection.jsx
+++ src/components/TasksSection/TasksSection.jsx
@@ -1,6 +1,9 @@
 import SecondaryButton from "../buttons/SecondaryButton/SecondaryButton";
 import SortingIcon from "../icons/SortingIcon/SortingIcon";
 import "./TasksSection.scss";
+import Input from "../inputs/Input/Input";
+import PlusIcon from "../icons/PlusIcon/PlusIcon";
+import Checkbox from "../controls/Checkbox/Checkbox";
 
 export default function TasksSection() {
   return (
@@ -13,8 +16,15 @@ export default function TasksSection() {
       </div>
       <div className={"tasks"}>
         <div className={"tasks__header"}>
-          <p className={'tasks__headline'}>задача</p>
-          <p className={'tasks__headline'}>время</p>
+          <p className={"tasks__headline"}>задача</p>
+          <p className={"tasks__headline"}>время</p>
+        </div>
+        <Input>
+          <PlusIcon />
+        </Input>
+        <div className={"tasks__task"}>
+            <p className={'task__title-wrapper'}><Checkbox /><span className={'task__title'}>Провести митап</span></p>
+            <p className={'task__time'}>01:15:00</p>
         </div>
       </div>
     </div>
